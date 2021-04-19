588d66f879b1881ccc337475f0464b9fe7a0017d
yeqfx <yeqfx@hotmail.com>
2021-04-19 14:10:02 +0900

    Lecture 03 프로그램의 기본 형태와 Method

---
+++ chap03_Method/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace chap03_Method
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBoxResult = new System.Windows.Forms.TextBox();
+            this.tBoxNum1 = new System.Windows.Forms.TextBox();
+            this.tBoxNum2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tBoxResult
+            // 
+            this.tBoxResult.Enabled = false;
+            this.tBoxResult.Location = new System.Drawing.Point(33, 21);
+            this.tBoxResult.Name = "tBoxResult";
+            this.tBoxResult.Size = new System.Drawing.Size(315, 23);
+            this.tBoxResult.TabIndex = 0;
+            this.tBoxResult.Text = "0";
+            // 
+            // tBoxNum1
+            // 
+            this.tBoxNum1.Location = new System.Drawing.Point(33, 60);
+            this.tBoxNum1.Name = "tBoxNum1";
+            this.tBoxNum1.Size = new System.Drawing.Size(153, 23);
+            this.tBoxNum1.TabIndex = 1;
+            this.tBoxNum1.Text = "0";
+            // 
+            // tBoxNum2
+            // 
+            this.tBoxNum2.Location = new System.Drawing.Point(192, 59);
+            this.tBoxNum2.Name = "tBoxNum2";
+            this.tBoxNum2.Size = new System.Drawing.Size(156, 23);
+            this.tBoxNum2.TabIndex = 2;
+            this.tBoxNum2.Text = "0";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(30, 97);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 66);
+            this.btnPlus.TabIndex = 3;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(111, 97);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 66);
+            this.btnMinus.TabIndex = 4;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(192, 97);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(75, 66);
+            this.btnMulti.TabIndex = 5;
+            this.btnMulti.Text = "*";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Location = new System.Drawing.Point(273, 97);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(75, 66);
+            this.btnDivision.TabIndex = 6;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 185);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.tBoxNum2);
+            this.Controls.Add(this.tBoxNum1);
+            this.Controls.Add(this.tBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBoxResult;
+        private System.Windows.Forms.TextBox tBoxNum1;
+        private System.Windows.Forms.TextBox tBoxNum2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDivision;
+    }
+}
+
---
+++ chap03_Method/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chap03_Method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tBoxNum1.Text);
+            int iNumB = int.Parse(tBoxNum2.Text);
+
+            tBoxResult.Text = fPlus(iNumA, iNumB).ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tBoxNum1.Text);
+            int iNumB = int.Parse(tBoxNum2.Text);
+
+            tBoxResult.Text = fMinus(iNumA, iNumB).ToString();
+        }
+
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tBoxNum1.Text);
+            int iNumB = int.Parse(tBoxNum2.Text);
+
+            tBoxResult.Text = fMulti(iNumA, iNumB).ToString();
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            int iNumA = int.Parse(tBoxNum1.Text);
+            int iNumB = int.Parse(tBoxNum2.Text);
+
+            tBoxResult.Text = fDivision(iNumA, iNumB).ToString();
+        }
+
+        private int fPlus(int iA, int iB)
+        {
+            int iResult = 0;
+
+            iResult = iA + iB;
+
+            return iResult;
+        }
+
+        private int fMinus(int iA, int iB)
+        {
+            int iResult = 0;
+
+            iResult = iA - iB;
+
+            return iResult;
+        }
+
+        private int fMulti(int iA, int iB)
+        {
+            int iResult = 0;
+
+            iResult = iA * iB;
+
+            return iResult;
+        }
+
+        private int fDivision(int iA, int iB)
+        {
+            int iResult = 0;
+
+            iResult = iA / iB;
+
+            return iResult;
+        }
+    }
+}
