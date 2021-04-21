3e4b02be9ac2a39c2bae94d5e67ff4bc8aef80dc
yeqfx <yeqfx@hotmail.com>
2021-04-21 14:19:48 +0900

    Lecture 07 Array(배열)

---
+++ chap07_Array/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace chap07_Array
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgDay = new System.Windows.Forms.DataGridView();
+            this.colDay1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDay7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblArrayCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgDay
+            // 
+            this.dgDay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDay.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colDay1,
+            this.colDay2,
+            this.colDay3,
+            this.colDay4,
+            this.colDay5,
+            this.colDay6,
+            this.colDay7});
+            this.dgDay.Location = new System.Drawing.Point(12, 12);
+            this.dgDay.Name = "dgDay";
+            this.dgDay.RowTemplate.Height = 25;
+            this.dgDay.Size = new System.Drawing.Size(749, 150);
+            this.dgDay.TabIndex = 0;
+            // 
+            // colDay1
+            // 
+            this.colDay1.HeaderText = "월";
+            this.colDay1.Name = "colDay1";
+            // 
+            // colDay2
+            // 
+            this.colDay2.HeaderText = "화";
+            this.colDay2.Name = "colDay2";
+            // 
+            // colDay3
+            // 
+            this.colDay3.HeaderText = "수";
+            this.colDay3.Name = "colDay3";
+            // 
+            // colDay4
+            // 
+            this.colDay4.HeaderText = "목";
+            this.colDay4.Name = "colDay4";
+            // 
+            // colDay5
+            // 
+            this.colDay5.HeaderText = "금";
+            this.colDay5.Name = "colDay5";
+            // 
+            // colDay6
+            // 
+            this.colDay6.HeaderText = "토";
+            this.colDay6.Name = "colDay6";
+            // 
+            // colDay7
+            // 
+            this.colDay7.HeaderText = "일";
+            this.colDay7.Name = "colDay7";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "일주일간";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "이주일간";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblArrayCount
+            // 
+            this.lblArrayCount.AutoSize = true;
+            this.lblArrayCount.Location = new System.Drawing.Point(239, 181);
+            this.lblArrayCount.Name = "lblArrayCount";
+            this.lblArrayCount.Size = new System.Drawing.Size(93, 15);
+            this.lblArrayCount.TabIndex = 3;
+            this.lblArrayCount.Text = "전체 자료 수 : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 219);
+            this.Controls.Add(this.lblArrayCount);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgDay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgDay;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDay7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblArrayCount;
+    }
+}
+
---
+++ chap07_Array/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chap07_Array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgDay.Rows.Clear();
+
+            int[] iTest = { 10, 5, 30, 4, 15, 22, 18 };
+
+            lblArrayCount.Text = String.Format("전체 자료 수 : {0}", iTest.Length.ToString());
+
+            for (int i = 0; i < 7; i++) dgDay["colDay" + (i + 1), 0].Value = iTest[i];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dgDay.Rows.Clear();
+
+            int[,] iTest = { { 10, 5, 30, 4, 15, 22, 18 }, { 11, 15, 20, 14, 15, 23, 17 } };
+
+            lblArrayCount.Text = String.Format("전체 자료 수 : {0}", iTest.Length.ToString());
+
+            dgDay.Rows.Add();
+
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 7; j++)
+                    dgDay["colDay" + (j + 1), i].Value = iTest[i, j];
+        }
+    }
+}
