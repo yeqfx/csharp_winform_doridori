b40365988d04ecc71ea59d1817e93ed824077a6b
yeqfx <yeqfx@hotmail.com>
2021-04-23 12:53:57 +0900

    Lecture 10. 반복문2 (while, do while)

---
+++ chap10_while_do_while/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace chap10_while_do_while
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblwhileResult = new System.Windows.Forms.Label();
+            this.btnwhileResult = new System.Windows.Forms.Button();
+            this.lboxwhileResult = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btndowhileResult = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tboxNumber = new System.Windows.Forms.TextBox();
+            this.lbldowhileResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblwhileResult
+            // 
+            this.lblwhileResult.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblwhileResult.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblwhileResult.Location = new System.Drawing.Point(12, 9);
+            this.lblwhileResult.Name = "lblwhileResult";
+            this.lblwhileResult.Size = new System.Drawing.Size(198, 23);
+            this.lblwhileResult.TabIndex = 0;
+            this.lblwhileResult.Text = "0, 0, 0, 0, 0, 0,";
+            this.lblwhileResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnwhileResult
+            // 
+            this.btnwhileResult.Location = new System.Drawing.Point(216, 9);
+            this.btnwhileResult.Name = "btnwhileResult";
+            this.btnwhileResult.Size = new System.Drawing.Size(95, 23);
+            this.btnwhileResult.TabIndex = 2;
+            this.btnwhileResult.Text = "로또번호 생성";
+            this.btnwhileResult.UseVisualStyleBackColor = true;
+            this.btnwhileResult.Click += new System.EventHandler(this.btnwhileResult_Click);
+            // 
+            // lboxwhileResult
+            // 
+            this.lboxwhileResult.FormattingEnabled = true;
+            this.lboxwhileResult.ItemHeight = 15;
+            this.lboxwhileResult.Location = new System.Drawing.Point(12, 35);
+            this.lboxwhileResult.Name = "lboxwhileResult";
+            this.lboxwhileResult.Size = new System.Drawing.Size(198, 109);
+            this.lboxwhileResult.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Location = new System.Drawing.Point(12, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(299, 3);
+            this.panel1.TabIndex = 4;
+            // 
+            // btndowhileResult
+            // 
+            this.btndowhileResult.Location = new System.Drawing.Point(216, 192);
+            this.btndowhileResult.Name = "btndowhileResult";
+            this.btndowhileResult.Size = new System.Drawing.Size(95, 23);
+            this.btndowhileResult.TabIndex = 5;
+            this.btndowhileResult.Text = "선택번호 뽑기";
+            this.btndowhileResult.UseVisualStyleBackColor = true;
+            this.btndowhileResult.Click += new System.EventHandler(this.btndowhileResult_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1~100 안의 숫자를 입력하세요.";
+            // 
+            // tboxNumber
+            // 
+            this.tboxNumber.Location = new System.Drawing.Point(12, 192);
+            this.tboxNumber.Name = "tboxNumber";
+            this.tboxNumber.Size = new System.Drawing.Size(100, 23);
+            this.tboxNumber.TabIndex = 7;
+            this.tboxNumber.Text = "1";
+            // 
+            // lbldowhileResult
+            // 
+            this.lbldowhileResult.AutoSize = true;
+            this.lbldowhileResult.Location = new System.Drawing.Point(12, 231);
+            this.lbldowhileResult.Name = "lbldowhileResult";
+            this.lbldowhileResult.Size = new System.Drawing.Size(12, 15);
+            this.lbldowhileResult.TabIndex = 8;
+            this.lbldowhileResult.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 265);
+            this.Controls.Add(this.lbldowhileResult);
+            this.Controls.Add(this.tboxNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btndowhileResult);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lboxwhileResult);
+            this.Controls.Add(this.btnwhileResult);
+            this.Controls.Add(this.lblwhileResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblwhileResult;
+        private System.Windows.Forms.Button btnwhileResult;
+        private System.Windows.Forms.ListBox lboxwhileResult;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btndowhileResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tboxNumber;
+        private System.Windows.Forms.Label lbldowhileResult;
+    }
+}
+
---
+++ chap10_while_do_while/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chap10_while_do_while
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnwhileResult_Click(object sender, EventArgs e)
+        {
+            int[] iArray = new int[6];
+            int iCount = 0;
+
+            StringBuilder sb = new StringBuilder();
+            Random rd = new Random();
+
+            // iArray 가 다 안찼으면 루프 진행
+            while (Array.IndexOf(iArray, 0) != -1)
+            {
+                int iNumber = rd.Next(1, 46);
+
+                if (Array.IndexOf(iArray, iNumber) == -1)
+                {
+                    iArray[iCount] = iNumber;
+                    sb.Append(string.Format("{0}, ", iNumber));
+                    iCount++;
+                }
+            }
+            lblwhileResult.Text = sb.ToString();
+            lboxwhileResult.Items.Add(sb.ToString());
+        }
+
+        private void btndowhileResult_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+
+            int iNumber = int.Parse(tboxNumber.Text);
+
+            if (iNumber < 1 || iNumber > 100)
+            {
+                MessageBox.Show("1~100 사이의 숫자를 입력해 주세요.");
+                return;
+            }
+
+            int iCheckNumber = 0;
+            int iCount = 0;
+
+            do
+            {
+                iCheckNumber = rd.Next(1, 101);
+                iCount++;
+            } while (iNumber != iCheckNumber);
+            lbldowhileResult.Text = string.Format("- 뽑은 숫자 {0}을 {1}회 만에 찾았습니다.", iNumber, iCount);
+        }
+    }
+}
