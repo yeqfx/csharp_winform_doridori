e52aa2309df2e9a99e5f791e065f0f5f1a8ca6d4
yeqfx <yeqfx@hotmail.com>
2021-04-24 22:25:17 +0900

    Lecture 11 구조체와 클래스(Struct, Class)

---
+++ chap11_Struct_Class/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace chap11_Struct_Class
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pboxSun = new System.Windows.Forms.PictureBox();
+            this.pboxMoon = new System.Windows.Forms.PictureBox();
+            this.pboxStar = new System.Windows.Forms.PictureBox();
+            this.pboxNone = new System.Windows.Forms.PictureBox();
+            this.rdoPlayer1 = new System.Windows.Forms.RadioButton();
+            this.rdoPlayer2 = new System.Windows.Forms.RadioButton();
+            this.lboxResult1 = new System.Windows.Forms.ListBox();
+            this.lboxResult2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxSun)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxMoon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxStar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxNone)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "- Player는 한번 씩 돌아가면서 그림을 선택합니다.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(399, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "- 각 10회 진행 후 해, 달, 별의 숫자의 합이 가장 높인 사람이 승리입니다.";
+            // 
+            // pboxSun
+            // 
+            this.pboxSun.Image = global::chap11_Struct_Class.Properties.Resources._1;
+            this.pboxSun.Location = new System.Drawing.Point(12, 58);
+            this.pboxSun.Name = "pboxSun";
+            this.pboxSun.Size = new System.Drawing.Size(100, 121);
+            this.pboxSun.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pboxSun.TabIndex = 2;
+            this.pboxSun.TabStop = false;
+            this.pboxSun.Click += new System.EventHandler(this.pBoxSun_Click);
+            // 
+            // pboxMoon
+            // 
+            this.pboxMoon.Image = global::chap11_Struct_Class.Properties.Resources._2;
+            this.pboxMoon.Location = new System.Drawing.Point(118, 58);
+            this.pboxMoon.Name = "pboxMoon";
+            this.pboxMoon.Size = new System.Drawing.Size(100, 121);
+            this.pboxMoon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pboxMoon.TabIndex = 3;
+            this.pboxMoon.TabStop = false;
+            this.pboxMoon.Click += new System.EventHandler(this.pboxMoon_Click);
+            // 
+            // pboxStar
+            // 
+            this.pboxStar.Image = global::chap11_Struct_Class.Properties.Resources._3;
+            this.pboxStar.Location = new System.Drawing.Point(224, 58);
+            this.pboxStar.Name = "pboxStar";
+            this.pboxStar.Size = new System.Drawing.Size(100, 121);
+            this.pboxStar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pboxStar.TabIndex = 4;
+            this.pboxStar.TabStop = false;
+            this.pboxStar.Click += new System.EventHandler(this.pboxStar_Click);
+            // 
+            // pboxNone
+            // 
+            this.pboxNone.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pboxNone.Location = new System.Drawing.Point(330, 58);
+            this.pboxNone.Name = "pboxNone";
+            this.pboxNone.Size = new System.Drawing.Size(100, 121);
+            this.pboxNone.TabIndex = 5;
+            this.pboxNone.TabStop = false;
+            this.pboxNone.Click += new System.EventHandler(this.pboxNone_Click);
+            // 
+            // rdoPlayer1
+            // 
+            this.rdoPlayer1.AutoSize = true;
+            this.rdoPlayer1.Location = new System.Drawing.Point(12, 202);
+            this.rdoPlayer1.Name = "rdoPlayer1";
+            this.rdoPlayer1.Size = new System.Drawing.Size(64, 19);
+            this.rdoPlayer1.TabIndex = 6;
+            this.rdoPlayer1.TabStop = true;
+            this.rdoPlayer1.Text = "Player1";
+            this.rdoPlayer1.UseVisualStyleBackColor = true;
+            // 
+            // rdoPlayer2
+            // 
+            this.rdoPlayer2.AutoSize = true;
+            this.rdoPlayer2.Location = new System.Drawing.Point(346, 202);
+            this.rdoPlayer2.Name = "rdoPlayer2";
+            this.rdoPlayer2.Size = new System.Drawing.Size(64, 19);
+            this.rdoPlayer2.TabIndex = 7;
+            this.rdoPlayer2.TabStop = true;
+            this.rdoPlayer2.Text = "Player2";
+            this.rdoPlayer2.UseVisualStyleBackColor = true;
+            // 
+            // lboxResult1
+            // 
+            this.lboxResult1.FormattingEnabled = true;
+            this.lboxResult1.ItemHeight = 15;
+            this.lboxResult1.Location = new System.Drawing.Point(12, 227);
+            this.lboxResult1.Name = "lboxResult1";
+            this.lboxResult1.Size = new System.Drawing.Size(312, 199);
+            this.lboxResult1.TabIndex = 8;
+            // 
+            // lboxResult2
+            // 
+            this.lboxResult2.FormattingEnabled = true;
+            this.lboxResult2.ItemHeight = 15;
+            this.lboxResult2.Location = new System.Drawing.Point(346, 227);
+            this.lboxResult2.Name = "lboxResult2";
+            this.lboxResult2.Size = new System.Drawing.Size(312, 199);
+            this.lboxResult2.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 440);
+            this.Controls.Add(this.lboxResult2);
+            this.Controls.Add(this.lboxResult1);
+            this.Controls.Add(this.rdoPlayer2);
+            this.Controls.Add(this.rdoPlayer1);
+            this.Controls.Add(this.pboxNone);
+            this.Controls.Add(this.pboxStar);
+            this.Controls.Add(this.pboxMoon);
+            this.Controls.Add(this.pboxSun);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pboxSun)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxMoon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxStar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxNone)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pboxSun;
+        private System.Windows.Forms.PictureBox pboxMoon;
+        private System.Windows.Forms.PictureBox pboxStar;
+        private System.Windows.Forms.PictureBox pboxNone;
+        private System.Windows.Forms.RadioButton rdoPlayer1;
+        private System.Windows.Forms.RadioButton rdoPlayer2;
+        private System.Windows.Forms.ListBox lboxResult1;
+        private System.Windows.Forms.ListBox lboxResult2;
+    }
+}
+
