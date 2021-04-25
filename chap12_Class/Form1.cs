cb32606e0b1172677bd19298b19aa0c2eb6cf34f
yeqfx <yeqfx@hotmail.com>
2021-04-25 17:28:52 +0900

    Lecture 12 클래스의 생성 및 사용 (Class)

---
+++ chap12_Class/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chap12_Class
+{
+    public partial class Form1 : Form
+    {
+        Random _rd = new Random();
+
+        structPlayer _sp1;
+        structPlayer _sp2;
+
+        Player _cPlayer = new Player();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pboxSun_Click(object sender, EventArgs e)
+        {
+            int iNumber = _rd.Next(1, 21);
+
+            if (rdoPlayer1.Checked)
+            {
+                _sp1.iSun = iNumber;
+            }
+            else
+            {
+                _sp2.iSun = iNumber;
+            }
+
+            Result();
+        }
+
+        private void pboxMoon_Click(object sender, EventArgs e)
+        {
+            int iNumber = _rd.Next(1, 21);
+
+            if (rdoPlayer1.Checked)
+            {
+                _sp1.iMoon = iNumber;
+            }
+            else
+            {
+                _sp2.iMoon = iNumber;
+            }
+
+            Result();
+        }
+
+        private void pboxStar_Click(object sender, EventArgs e)
+        {
+            int iNumber = _rd.Next(1, 21);
+
+            if (rdoPlayer1.Checked)
+            {
+                _sp1.iStar = iNumber;
+            }
+            else
+            {
+                _sp2.iStar = iNumber;
+            }
+
+            Result();
+        }
+
+        private void pboxNone_Click(object sender, EventArgs e)
+        {
+            Result();
+        }
+
+        private void iCheckedChange()
+        {
+            if (rdoPlayer1.Checked)
+            {
+                rdoPlayer2.Checked = true;
+            }
+            else
+            {
+                rdoPlayer1.Checked = true;
+            }
+        }
+
+        private void Result()
+        {
+            string strResult = string.Empty;
+
+            if (rdoPlayer1.Checked)
+            {
+                _sp1.iCount++;
+                _sp1.iCardSum = _cPlayer.CardSum(_sp1.iSun, _sp1.iMoon, _sp1.iStar);
+                strResult = _cPlayer.ResultText(_sp1.iCount, _sp1.iSun, _sp1.iMoon, _sp1.iStar, _sp1.iCardSum);
+                lboxResult1.Items.Add(strResult);
+            }
+            else
+            {
+                _sp2.iCount++;
+                _sp2.iCardSum = _cPlayer.CardSum(_sp2.iSun, _sp2.iMoon, _sp2.iStar);
+                strResult = _cPlayer.ResultText(_sp2.iCount, _sp2.iSun, _sp2.iMoon, _sp2.iStar, _sp2.iCardSum);
+                lboxResult2.Items.Add(strResult);
+            }
+
+            iCheckedChange();
+
+            if (_sp1.iCount == _sp2.iCount)
+            {
+                lboxNow.Items.Add(_cPlayer.PlayerPair(_sp2.iCount, _sp1.iCardSum, _sp2.iCardSum));
+
+                if (_sp2.iCount >= 5)
+                {
+                    lboxNow.Items.Add(_cPlayer.PlayerResult(_sp1.iCardSum, _sp2.iCardSum));
+                }
+            }
+        }
+    }
+}
---
+++ chap12_Class/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap12_Class
+{
+    struct structPlayer
+    {
+        public int iCount;
+
+        public int iSun;
+        public int iMoon;
+        public int iStar;
+
+        public int iCardSum;
+    }
+    class Player
+    {
+        public int CardSum(int iSun, int iMoon, int iStar)
+        {
+            return iSun + iMoon + iStar;
+        }
+
+        public string ResultText(int iCount, int iSun, int iMoon, int iStar, int iCardSum)
+        {
+            return string.Format("{0}회 해:{1}, 달:{2}, 별:{3} => 합계는 {4}입니다.", iCount, iSun, iMoon, iStar, iCardSum);
+        }
+
+        public string PlayerPair(int iCount, int iP1CardSum, int iP2CardSum)
+        {
+            int iCheck = iP1CardSum - iP2CardSum;
+
+            if (iCheck > 0)
+            {
+                return string.Format("{0}회차 : Player1이 {1} 만큼 더 큽니다.", iCount, iCheck);
+            }
+            else if (iCheck < 0)
+            {
+                return string.Format("{0}회차 : Player2가 {1} 만큼 더 큽니다.", iCount, iCheck * -1);
+            }
+            else
+            {
+                return string.Format("{0}회차 : 둘의 값이 같습니다.", iCount);
+            }
+        }
+
+        public string PlayerResult(int iP1CardSum, int iP2CardSum)
+        {
+            string strReturn = string.Empty;
+
+            if (iP1CardSum > iP2CardSum)
+            {
+                strReturn = "Player1이 이겼습니다.";
+            }
+            else if (iP1CardSum < iP2CardSum)
+            {
+                strReturn = "Player2가 이겼습니다.";
+            }
+            else
+            {
+                strReturn = "비겼습니다.";
+            }
+
+            return strReturn;
+        }
+    }
+}
